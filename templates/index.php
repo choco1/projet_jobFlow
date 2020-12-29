a6c1bf4a51fa57ac939cd62df522331d49f98954
najat ciesielczyk <ro-se_no-ire@hotmail.fr>
2020-12-29 14:35:51 +0100

    sreach

--- templates/index.php
+++ templates/index.php
@@ -1,5 +1,19 @@
 <?php require '../partials/navBar.php'; ?>
 
-<div>
-    <img  src="../fonts/image.jpg" alt="">
+<div class=" bg-laptop h-96 bg-cover bg-center">
+    <h1 class="title ">Votre Avenir Profetionnel est avec Nous</h1>
+</div>
+
+
+
+<div class="search-box">     
+    <div class="input-box">
+        <input type="checkbox" id="check">
+        <div class="box">
+            <input type="text" placeholder="Métier recherché (ex: cuisinier)">
+            <input type="text" class="city" placeholder="Lieu: ville, code postal ou région">
+            <label id="label" for="check"><img class="iconSearch" src="../fonts/search-solid.svg" alt=""></label>
+            
+        </div>
+    </div>       
 </div>
